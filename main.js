a82ed5e12a353990997bae739dbe8b49b9936e95
Vivekpandey76 <Vivekpandey22982@gmail.com>
2021-01-21 20:17:10 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log(" Hello world")
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 2000,
+  wrap: false
+})
+let btn=document.getElementById('btn');
+btn.addEventListener('click',(event)=>{
+    alert("Hey sorry for inconvience as of now this is just design template I didn't update it contact part much so i will update it soon");
+})
+
